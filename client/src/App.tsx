e38c1e8550c41c84716a9711ce6756d003c5d0b6
Jacob Matthews <jacob@jacobmatthe.ws>
2025-01-18 20:10:38 +1100

    Update with ssl and routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,54 +7,53 @@ import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport'
 import { Greeting } from './protos/dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [greeting, setGreeting] = useState<Greeting | undefined>(undefined);
+    const [count, setCount] = useState(0)
+    const [greeting, setGreeting] = useState<Greeting | undefined>(undefined);
 
-  const transport = new GrpcWebFetchTransport({
-    baseUrl: "http://localhost:8070",
-    format: 'binary'
-  });
-  const client = new DashboardClient(transport);
+    const transport = new GrpcWebFetchTransport({
+        baseUrl: `https://${window.location.hostname}/api/v1/grpc`,
+    });
+    const client = new DashboardClient(transport);
 
-  useEffect(() => {
-    client.getGreeting({}).then(({ response }) => {
-      setGreeting(response.greeting);
-    }).catch((err) => {
-      console.log(err);
-    })
-  }, [])
+    useEffect(() => {
+        client.getGreeting({}).then(({ response }) => {
+            setGreeting(response.greeting);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }, [])
 
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      {greeting && (
-        <div>
-          <pre>{greeting.id}</pre>
-          <p>{greeting.message}</p>
-        </div>
-      )}
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+            {greeting && (
+                <div>
+                    <pre>{greeting.id}</pre>
+                    <p>{greeting.message}</p>
+                </div>
+            )}
+        </>
+    )
 }
 
 export default App
--- server/main.go
+++ server/main.go
@@ -2,30 +2,47 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net"
+	"net/http"
+	"sync"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
 	"jacobmatthe.ws/grpc-web-test/protos/dashboard"
 )
 
-type service struct {
-	dashboard.UnimplementedDashboardServer
-}
-
-func (s *service) GetGreeting(ctx context.Context, r *dashboard.GetGreetingRequest) (*dashboard.GetGreetingResponse, error) {
-	return &dashboard.GetGreetingResponse{
-		Greeting: &dashboard.Greeting{
-			Id:      "37bed5cb-bc49-41d3-95cf-182469751da6",
-			Message: "Hello there!",
-		},
-	}, nil
-
-}
-
 func main() {
-	lis, err := net.Listen("tcp", ":9090")
+	wg := &sync.WaitGroup{}
+	println("started")
+	wg.Add(2)
+	go runGrpc(":9090", wg)
+	go runHttp(":9091", wg)
+	wg.Wait()
+}
+
+func runHttp(addr string, wg *sync.WaitGroup) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("howdy"))
+	})
+
+	server := &http.Server{
+		Addr: addr,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		wg.Done()
+		panic(err)
+	}
+}
+
+func runGrpc(addr string, wg *sync.WaitGroup) {
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
+		wg.Done()
 		panic(err)
 	}
 
@@ -35,9 +52,26 @@ func main() {
 	dashboard.RegisterDashboardServer(s, service)
 	reflection.Register(s)
 
-	println("listening")
+	println("grpc listening")
 
 	if err := s.Serve(lis); err != nil {
+		wg.Done()
 		panic(err)
 	}
+}
+
+type service struct {
+	dashboard.UnimplementedDashboardServer
+}
+
+func (s *service) GetGreeting(ctx context.Context, r *dashboard.GetGreetingRequest) (*dashboard.GetGreetingResponse, error) {
+	md, _ := metadata.FromIncomingContext(ctx)
+	fmt.Printf("%#v\n", md)
+	return &dashboard.GetGreetingResponse{
+		Greeting: &dashboard.Greeting{
+			Id:      "37bed5cb-bc49-41d3-95cf-182469751da6",
+			Message: "Hello there!",
+		},
+	}, nil
+
 }
